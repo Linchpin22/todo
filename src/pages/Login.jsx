e398bcb74082f5a0ca0fd6b1f207bbdbe8abeafd
Linchpin22 <tiwarianuj712u@gmail.com>
2024-08-27 21:03:39 +0530

    changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -30,7 +30,7 @@ function Login() {
     <>
       <form onSubmit={handelLogin} className='mt-24 flex flex-col  gap-4 w-[400px] mx-auto'>
         <h1 className='text-center font-extrabold text-xl my-4'>Login</h1>
-        <input ref={email} className='border-2 theme-bg-gray rounded px-2 py-px border-gray-500/30' type="email" required name='email' placeholder='ibrar@email.com' />
+        <input ref={email} className='border-2 theme-bg-gray rounded px-2 py-px border-gray-500/30' type="email" required name='email' placeholder='anuj@email.com' />
         <input ref={password} className='border-2 theme-bg-gray rounded px-2 py-px border-gray-500/30' type="password" required name='password' placeholder='password' />
         <button type='submit' className='theme-bg-i rounded text-lg font-semibold p-1 ' >Login</button>
         <p className='w-max mx-auto  text-red-600'>{error}</p>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -28,7 +28,7 @@ function Signup() {
       <form onSubmit={handelSignup} className='mt-24 flex flex-col  gap-4 w-[400px] mx-auto'>
         <h1 className='text-center font-extrabold text-xl my-4'>Signup</h1>
         <input ref={username} className='border-2 theme-bg-gray rounded px-2 py-px border-gray-500/30' type="text" required name='username'   placeholder='Username' />
-        <input ref={email} className='border-2 theme-bg-gray rounded px-2 py-px border-gray-500/30' type="email" required name='email' autoComplete='username'  placeholder='user@email.com' />
+        <input ref={email} className='border-2 theme-bg-gray rounded px-2 py-px border-gray-500/30' type="email" required name='email' autoComplete='username'  placeholder='anuj@email.com' />
         <input ref={password} className='border-2 theme-bg-gray rounded px-2 py-px border-gray-500/30' type="password" required name='password' placeholder='password' />
         <button type='submit' className='theme-bg-i rounded text-lg font-semibold p-1 ' >Signup</button>
       </form>
